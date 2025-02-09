add8c4022140a81b81c860bac7b1733811f5ba4f
jeremiahvaughan <jeremiah.t.vaughan@gmail.com>
2025-02-08 19:22:01 -0600

    test

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/JeremiahVaughan/key-sender
 
 go 1.23.4
 
-require github.com/stianeikeland/go-rpio/v4 v4.6.0
+require (
+	github.com/warthog618/go-gpiocdev v0.9.1
+	github.com/warthog618/go-gpiosim v0.1.1
+)
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,33 +5,44 @@ import (
 	"sync"
 	"time"
 
-	"github.com/stianeikeland/go-rpio/v4"
+	"github.com/warthog618/go-gpiocdev"
+	"github.com/warthog618/go-gpiocdev/device/rpi"
 )
 
 func main() {
 	log.Println("program started")
-	err := rpio.Open()
+	c, err := gpiocdev.NewChip("/dev/gpiochip0")
 	if err != nil {
-		log.Fatalf("error, unable to open rpio. Error: %v", err)
+		log.Fatalf("error, when NewChip() for main(). Error: %v", err)
 	}
-	defer rpio.Close()
+	l, err := c.RequestLine(rpi.GPIO16, gpiocdev.WithEventHandler(handler), gpiocdev.WithRisingEdge)
+	if err != nil {
+		log.Fatalf("error, when RequestLine() for main(). Error: %v", err)
+	}
+	defer l.Close()
+
+	// err := rpio.Open()
+	// if err != nil {
+	// 	log.Fatalf("error, unable to open rpio. Error: %v", err)
+	// }
+	// defer rpio.Close()
 
 	// crashing
 	// pin := rpio.Pin(25) // physical 22
-	pin := rpio.Pin(16) // physical 36
-	pin.Input()
-	pin.PullUp()
-	pin.Detect(rpio.FallEdge)
-	pollRate := time.Second
-	d := newDebouncer(time.Second * 3)
-	for {
-		log.Println("polling")
-		if pin.EdgeDetected() {
-			log.Println("edge detected")
-			d.debounce(handleButtonPress)
-		}
-		time.Sleep(pollRate)
-	}
+	// pin := rpio.Pin(16) // physical 36
+	// pin.Input()
+	// pin.PullUp()
+	// pin.Detect(rpio.FallEdge)
+	// pollRate := time.Second
+	// d := newDebouncer(time.Second * 3)
+	// for {
+	// 	log.Println("polling")
+	// 	if pin.EdgeDetected() {
+	// 		log.Println("edge detected")
+	// 		d.debounce(handleButtonPress)
+	// 	}
+	// 	time.Sleep(pollRate)
+	// }
 }
 
 func handleButtonPress() {
@@ -69,4 +80,9 @@ func (d *debouncer) debounce(f func()) {
 		d.timer = nil // Allow the next function call to use a new timer
 		d.mu.Unlock()
 	})
+}
+
+func handler(evt gpiocdev.LineEvent) {
+	// handle edge event
+	log.Println("edge detected")
 }
